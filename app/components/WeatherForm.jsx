9fde4e1ea83c27c0b0c135fd613336925e52360f
Mladen <mladja92@yahoo.com>
2017-02-09 19:06:43 +0100

    Add styles to weather page

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -15,8 +15,8 @@ const WeatherForm = React.createClass({
     return (
       <div>
         <form onSubmit={this.onFormSubmit}>
-          <input type="text" ref="location" placeholder="Enter City Name"/>
-          <button>Get Weather</button>
+          <input type="text" ref="location" placeholder="Enter City Name..."/>
+          <button className="button expanded hollow">Get Weather</button>
         </form>
       </div>
     )
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ const React = require('react');
 
 const WeatherMessage = ({temp, location}) => {
   return (
-    <h3>it is {temp} in {location}</h3>
+    <h3 className="text-center">It's {temp}&deg;C in {location}</h3>
   );
 };
 
